a64015aa7e3da253ecc868e452d79f82bcca6084
kiku98 <info@kiliankunkel.de>
2024-03-19 15:44:10 -0500

    Rep finished

--- typescript-jest/src/TennisGame.ts
+++ typescript-jest/src/TennisGame.ts
@@ -7,25 +7,58 @@ export interface ScoreRepresentation {
 
 export class NormalRepresentation implements ScoreRepresentation {
   getScore(game: TennisGame): string {
-    throw new Error('Method not implemented.');
+    const scoreNames = ['Love', 'Fifteen', 'Thirty', 'Forty'];
+    return `${scoreNames[game.player1.getScore()]}-${scoreNames[game.player2.getScore()]}`;
   }
 }
 
 export class DrawRepresentation implements ScoreRepresentation {
   getScore(game: TennisGame): string {
-    throw new Error('Method not implemented.');
+    const scoreNames = ['Love', 'Fifteen', 'Thirty'];
+    const scoreMap: { [key: number]: string } = {
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      0: `${scoreNames[0]}-All`,
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      1: `${scoreNames[1]}-All`,
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      2: `${scoreNames[2]}-All`,
+    };
+    return scoreMap[game.player1.getScore()] || 'Deuce';
   }
 }
 
 export class EndRepresentation implements ScoreRepresentation {
   getScore(game: TennisGame): string {
-    throw new Error('Method not implemented.');
+    const difference: number =
+      game.player1.getScore() - game.player2.getScore();
+
+    const leader: string =
+      difference > 0 ? game.player1.getName() : game.player2.getName();
+
+    if (Math.abs(difference) === 1) {
+      return `Advantage ${leader}`;
+    } else {
+      return `Win for ${leader}`;
+    }
   }
 }
 
 export class GameRepresentation implements ScoreRepresentation {
+  normalRepresentation = new NormalRepresentation();
+  drawRepresentationo = new DrawRepresentation();
+  endRepresentation = new EndRepresentation();
+
   getScore(game: TennisGame): string {
-    throw new Error('Method not implemented.');
+    const player1Score = game.player1.getScore();
+    const player2Score = game.player2.getScore();
+
+    if (player1Score == player2Score) {
+      return this.drawRepresentationo.getScore(game);
+    } else if (Math.max(player1Score, player2Score) > 4) {
+      return this.endRepresentation.getScore(game);
+    } else {
+      return this.normalRepresentation.getScore(game);
+    }
   }
 }
 
